b2f661a3b62e6fd7841d79365888244dbc64ba31
Ahmed Baz <developer.baz@gmail.com>
2023-12-31 10:50:26 +0200

    update UserService and UserController

--- src/main/java/com/sme/app/service/UserService.java
+++ src/main/java/com/sme/app/service/UserService.java
@@ -2,10 +2,18 @@ package com.sme.app.service;
 
 import com.sme.app.vo.UserVo;
 
+import java.util.List;
+
 public interface UserService {
 
     UserVo findById(Long id);
 
+    List<UserVo> findAll();
+
     UserVo addUser(UserVo user);
+
+    UserVo updateUser(Long id, UserVo user);
+
+    Long deleteUser(Long id);
 
 }
